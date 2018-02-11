a2b82c44b75e680ae9ed71c00dd92ffac5b3a894
ShawnaVacianna <shawnkay.vacianna@mymona.uwi.edu>
2018-02-11 00:41:58 +0000

    lab 2 exercise done

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
+import datetime
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
@@ -24,7 +24,14 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
-
+@app.route('/profile')
+def profile():
+    def format_date_joined():
+        date_joined = datetime.date(2018,2,7)
+        return date_joined.strftime('%b, %Y')
+    """Render the fake profile information page."""
+    return render_template('profile.html', date=format_date_joined())
+    
 ###
 # The functions below should be applicable to all Flask apps.
 ###
